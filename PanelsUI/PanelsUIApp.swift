ebce9612ad32823099c45c0604b25ab206b06994
Arbaz Kaladiya <kaladiya.arbaz@gmail.com>
2024-12-08 12:03:15 +0530

    Initial Commit

---
+++ PanelsUI/PanelsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PanelsUIApp.swift
+//  PanelsUI
+//
+//  Created by Arbaz Kaladiya on 08/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PanelsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PanelsUITests/PanelsUITests.swift
@@ -0,0 +1,16 @@
+//
+//  PanelsUITests.swift
+//  PanelsUITests
+//
+//  Created by Arbaz Kaladiya on 08/12/24.
+//
+
+import Testing
+
+struct PanelsUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
